58fd26aa9257b2cbdfb328f14298d3b8926b70c9
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2024-01-27 21:49:21 +0100

    update ThemeSwitcher.tsx -- fix issue of default mode

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -12,7 +12,9 @@ export default function Switcher() {
     <div className="p-[8px] transition bg-white dark:bg-darkbg dark:border dark:border-[#555] flex flex-col gap-3 w-fit align-center rounded-[100px] bg-white">
       <button
         className={`${
-          theme === "light" ? "bg-primary" : "bg-transparent"
+          theme === "light" || theme === "undefined"
+            ? "bg-primary"
+            : "bg-transparent"
         } flex align-center justify-center w-[30px] px-[7.5px] py-[6.563px] rounded-full
         transition-colors ease-in-out duration-[0.5s]
         `}
