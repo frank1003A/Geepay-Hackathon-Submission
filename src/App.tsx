99c988427333cc2f336845e01b459a9bee31bdd8
Frank Ezene <63779386+frank1003A@users.noreply.github.com>
2024-01-27 23:23:03 +0100

    update App.tsx and Sectionheader.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Date from "./components/Date";
 import Search from "./components/Search";
 import ChartCard from "./components/Ui/Dashboard/ChartCard";
@@ -8,21 +9,37 @@ import TableCard from "./components/Ui/Dashboard/TableCard";
 import Layout from "./layout";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <Layout>
-      <div className="py-2 w-full md:hidden">
-        <Date />
-      </div>
-      <div className="flex items-center justify-start py-[0px] md:py-[20px] px-4 lg:hidden">
-        <Search />
-      </div>
-      <PrimaryGrid>
-        <ChartCard />
-        <Stats />
-        <TableCard />
-        <PlatformsCard />
-      </PrimaryGrid>
-    </Layout>
+    <>
+      {loading ? (
+        <div className="flex items-center justify-center fixed inset-0 bg-white">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <Layout>
+          <div className="py-2 w-full md:hidden">
+            <Date />
+          </div>
+          <div className="flex items-center justify-start py-[0px] md:py-[20px] px-4 lg:hidden">
+            <Search />
+          </div>
+          <PrimaryGrid>
+            <ChartCard />
+            <Stats />
+            <TableCard />
+            <PlatformsCard />
+          </PrimaryGrid>
+        </Layout>
+      )}
+    </>
   );
 }
 
--- src/components/Ui/Dashboard/Sectionheader.tsx
+++ src/components/Ui/Dashboard/Sectionheader.tsx
@@ -6,7 +6,7 @@ interface SectionheaderProps {
 }
 const Sectionheader = ({ title, children }: SectionheaderProps) => {
   return (
-    <div className="flex w-full items-center justify-between text-[18px] dark:text-white sticky top-0 bg-white dark:bg-carddark pl-[20px] pr-[16px]">
+    <div className="flex w-full items-center justify-between text-[18px] dark:text-white sticky top-0 left-0 bg-white dark:bg-carddark pl-[20px] pr-[16px]">
       <h1 className="font-bold text-primaryfont text-[#26282C] dark:text-white">
         {title}
       </h1>
